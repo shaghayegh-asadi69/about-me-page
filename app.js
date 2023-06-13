f56ae11fe15b9c888b14ddf417c6bcaaf89354eb
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-06-13 20:02:23 +0100

    second-commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 "use strict";
+let points = 0;
+let btn = document.getElementById("btn");
 
 let userName = prompt("Heey what's your name?");
 alert("nice to meet you , " + userName);
 
-let userClub = prompt("Do you want to be a member in our club?!");
+let userClub = prompt("Do you want to be a member in our club?!").toLowerCase();
 //console.log(userClub);}
 if (userClub === "yes" || userClub === "y") {
   //console.log("nice to have you!");
@@ -12,28 +14,31 @@ if (userClub === "yes" || userClub === "y") {
   //console.log("no waay!");
   alert("no waaay!");
 
-let userJob = prompt("Are you currently a student?!");
+
+btn.addEventListener("click", function() {let user = prompt("Are you ready to go?").toLowerCase();
+if (user === "yes" || user === "y") {
+  //console.log("I bet you are a good one!");
+  alert("let's go for it!");
+} else if (user === "no" || user === "n");
+alert("I see!");
+
+
+let userJob = prompt("Are you currently a student?!").toLowerCase;
 if (userJob === "yes" || userJob === "y") {
   //console.log("I bet you are a good one!");
   alert("I bet you are a good one! " + userName);
 } else if (userJob === "no" || userJob === "n");
-  alert("Same here!");
+alert("Same here!");
 
 
-let userCity = prompt("Do you live in Liverpool?!");
+let userCity = prompt("Do you live in Liverpool?!").toLowerCase();
 if (userCity === "yes" || userCity === "y") {
   //console.log("heeya townsman!");
   alert("heeya townsman!");
-} else if (userCity === "no"|| userCity === "n");
-  //console.log("Same here!");
-  alert("I'm really sorry to hear that, " + userName);
+} else if (userCity === "no" || userCity === "n");
+//console.log("Same here!");
+alert("I'm really sorry to hear that, " + userName);
 
 
-let user = prompt("Are you ready to go?");
-if (user === "yes" || user === "y") {
-  //console.log("I bet you are a good one!");
-  alert("let's go for it!");
-} else if (user === "no"|| user === "n");
-  alert("I see!");
 
-
+})
